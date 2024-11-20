0be58ab0276812c8d37fa3cdacac6532bb482d1f
Lucas Alves <lucas.marquesalv24@gmail.com>
2024-11-19 22:32:11 -0300

    Aula dia 19/11

---
+++ Atividades_19-11.py
@@ -0,0 +1,76 @@
+                                        # Questão 1
+
+# for i in range(1,101):
+#     print(i)
+
+# for i in range(100,0, -1):
+#     print(i)
+
+                                        # Questão 2 
+
+# soma = 0
+
+# for i in range(1,101):
+
+#     soma += i
+
+# print(soma)
+
+                                        # Questão 3
+
+# num = int(input("Insira um numero: "))
+
+# for i in range(1,11,1):
+#     result = num * i
+#     print(result)
+
+
+                                        # Questão 4
+
+# for i in range(2,51,2):
+#     print(i)
+
+
+                                        # Questão 5
+
+# numPares = 0
+# numImpares = 0
+
+# numeros = [1,4,9,10,13,16,19,22,25,28,33,38]
+
+# for i in numeros :
+
+#     if i%2 == 0:
+#         numPares +=1
+#     else: 
+#         numImpares+=1
+
+# print(f'Existem {numImpares} numeros pares e {numImpares} numeros impares na lista')
+
+
+                                    # Questão 6
+
+# i = 1
+
+# while i != 11:
+#     print(i)
+#     i+=1
+
+                                    # Questão 7
+
+# i = 1
+
+# while i != 51:
+#     if i%2 == 0:
+#         print(i)
+#     i +=1
+
+
+                                    # Questão 8 
+
+# i = 1
+
+# while i != 51:
+#     if i%2 != 0:
+#         print(i)
+#     i +=1
